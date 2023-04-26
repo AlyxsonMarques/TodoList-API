ca706b34ad681f7ddb2ca616bc2e4b5784a14b15
Alyxson Marques <alyxson.marques0@gmail.com>
2023-04-25 21:22:38 -0300

    Added GetAllTasks, CreateTask and GetSingleTask operations

--- controllers/taskManager.js
+++ controllers/taskManager.js
@@ -1,13 +1,39 @@
-const getAllTasks = (req, res) => {
-  console.log('get');
+const path = require('path');
+const db = require(path.normalize('../database/databaseManager.js'));
+
+const getAllTasks = async (req, res) => {
+  try {
+    const queryResult = await db.query(
+      'SELECT * from tasks'
+    );
+    res.status(200).json(queryResult.rows);
+  } catch (err) {
+    res.status(500).send('Internal error');
+  }
 }
 
-const createTask = (req, res) => {
-  console.log('post')
+const createTask = async (req, res) => {
+  try {
+    const queryResult = await db.query(`
+      INSERT INTO tasks(task_title, task_description) VALUES('${req.body.task_title}', '${req.body.task_description}')
+    `);
+    res.status(200).end();
+
+  } catch (err) {
+    res.status(500).send('Internal error');
+  }
 }
 
-const getSingleTask = (req, res) => {
-  console.log('get id')
+const getSingleTask = async (req, res) => {
+  try {
+    const queryResult = await db.query(`
+      SELECT * FROM tasks WHERE id = ${req.params.id}
+    `)
+    res.status(200).json(queryResult.rows);
+
+  } catch (err) {
+    res.status(500).send('Internal error');
+  }
 }
 
 const updateTask = (req, res) => {
