17d4d7c3d6ed3ee950c903de0fa546a5ddfdd22c
Alyxson Marques <alyxson.marques0@gmail.com>
2023-04-26 16:11:00 -0300

    Finished all CRUD operations in controller

--- controllers/taskManager.js
+++ controllers/taskManager.js
@@ -32,18 +32,36 @@ const getSingleTask = async (req, res) => {
       SELECT * FROM tasks WHERE id = $1
     `, values)
     res.status(200).json(queryResult.rows);
-
+  
   } catch (err) {
     res.status(500).send('Internal error');
   }
 }
 
-const updateTask = (req, res) => {
-  console.log('patch')
+const updateTask = async (req, res) => {
+  try {
+    const values = [req.body.task_title, req.body.task_description, req.body.completed, req.params.id];
+    const queryResult = await db.query(`
+      UPDATE tasks SET task_title = $1, task_description = $2, completed = $3 WHERE id = $4
+    `, values)
+    res.status(200).json(queryResult.rows);
+  
+  } catch (err) {
+    res.status(500).end();
+  }
 }
 
-const deleteTask = (req, res) => {
-  console.log('delete')
+const deleteTask = async (req, res) => {
+  try {
+    const values = [req.params.id];
+    const queryResult = await db.query(`
+      DELETE FROM tasks WHERE id = $1
+    `, values)
+    res.status(200).json(queryResult.rows);
+
+  } catch (err) {
+    res.status(500).end();
+  }
 }
 
 module.exports = {
